0945d7973a04a3b7a1554b581a47c061333b6c64
rkonara <rajith.konara@zalando.de>
2023-01-16 14:33:41 +0100

    testing VA

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"github.com/arsham/rainbow/rainbow"
+	"github.com/hashicorp/go-getter"
 	"os"
 )
 
@@ -14,5 +15,6 @@ func main() {
 		Reader: os.Stdin,  // to read from
 		Writer: os.Stdout, // to write to
 	}
+	getter.Get("", "")
 	l.Paint() // will rainbow everything it reads from reader to writer.
 }
